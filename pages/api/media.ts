1647afd46e835f10925b56f32b5fd3562a2154ca
fsouza.andreia <fsouza.andreia@gmail.com>
2024-04-26 04:08:08 +0100

    initial commit :tada:

---
+++ pages/api/media.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import S3 from "aws-sdk/clients/s3";
+import { randomUUID } from "crypto";
+
+const s3 = new S3({
+  apiVersion: "2006-03-01",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
+  signatureVersion: "v4",
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const extension = (req.query.fileType as string).split("/")[1];
+  const contentType = (req.query.fileType as string)
+
+  const Key = `${randomUUID()}.${extension}`;
+
+  const s3Params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key,
+    Expires: 60,
+    ContentType: contentType,
+  };
+
+  const uploadUrl = s3.getSignedUrl("putObject", s3Params);
+
+  res.status(200).json({
+    uploadUrl,
+    key: Key,
+  });
+}
---
+++ pages/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import axios from "axios";
+import { ChangeEvent } from "react";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  const [fileName, setFileName] = useState<string | null>(null);
+
+  const uploadToS3 = async (e: ChangeEvent<HTMLFormElement>) => {
+    const formData = new FormData(e.target);
+    const file = formData.get("file");
+
+    if (!file) return null;
+    // @ts-ignore
+    const fileType = encodeURIComponent(file.type);
+    const { data } = await axios.get(`/api/media?fileType=${fileType}`);
+    const { uploadUrl, key } = data;
+
+    await axios.put(uploadUrl, file);
+
+    return key;
+  };
+
+  const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await uploadToS3(e);
+  };
+
+  const handleChangeFileName = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0].name) {
+      const name = e.target.files[0]?.name;
+      setFileName(name);
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.titleContainer}>
+        {fileName ? <img src={"/file.svg"} alt="File Icon" width={16} /> : null}
+        <h1 className={styles.title}>
+          {!fileName ? "Selecione um relatório e faça o upload" : fileName}
+        </h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.uploadContainer}>
+          <div className={styles.labelContainer}>
+            <label className={styles.label} htmlFor="contained-button-file">
+              <img
+                src={fileName ? "/folder-open.svg" : "/folder.svg"}
+                alt="Folder Icon"
+                width={fileName ? 36 : 32}
+                className={styles.folderIcon}
+              />
+              <p className={styles.labelText}>
+                {fileName ? fileName : "Selecionar Relatório"}
+              </p>
+            </label>
+          </div>
+          <input
+            className={styles.input}
+            id="contained-button-file"
+            type="file"
+            name="file"
+            onChange={handleChangeFileName}
+          />
+          <button
+            title="Fazer upload do relatório"
+            className={styles.uploadButton}
+            type="submit"
+            disabled={!fileName}
+          >
+            Upload
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default Home;
