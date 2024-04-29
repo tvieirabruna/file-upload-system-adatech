cd4caed1a79d62171632f4a58eefcb8911d29347
Bruna <brunavieira@nano.ufrj.br>
2024-04-29 20:27:50 -0300

    test: test access key deletion from media.ts

--- pages/api/media.ts
+++ pages/api/media.ts
@@ -3,8 +3,6 @@ import S3 from "aws-sdk/clients/s3";
 
 const s3 = new S3({
   apiVersion: "2006-03-01",
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_KEY,
   region: process.env.REGION,
   signatureVersion: "v4",
 });
